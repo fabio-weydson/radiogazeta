cd276e89c6ce710532d5563073442efcd9a0fe77
fabio-weydson <fabioweydson@gmail.com>
2015-07-04 23:14:16 -0300

    teste_backgruond_android

--- www/js/app.js
+++ www/js/app.js
@@ -25,6 +25,20 @@
             if(ons.platform.isIOS()){
                 $('.navigation-bar').css({'padding-top':"20px"});
             }
+               // Android customization
+    cordova.plugins.backgroundMode.setDefaults({ text:'Doing heavy tasks.'});
+    // Enable background mode
+    cordova.plugins.backgroundMode.enable();
+
+    // Called when background mode has been activated
+    cordova.plugins.backgroundMode.onactivate = function () {
+        setTimeout(function () {
+            // Modify the currently displayed notification
+            cordova.plugins.backgroundMode.configure({
+                text:'Running in background for more than 5s now.'
+            });
+        }, 5000);
+    }
             //$scope.ons.navigator.pushPage('radio2.html',{title : 'title'});
             StatusBar.styleBlackOpaque();
         });
